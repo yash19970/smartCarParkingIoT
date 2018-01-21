538b0ff26ef49cab18736148e81e0236733b48a8
divyang duhan <divyang.duhan07@gmail.com>
2018-01-21 04:21:41 -0600

    current time updating in firebase

--- Carpark/app/src/main/java/com/example/divyang/carpark/MainActivity.java
+++ Carpark/app/src/main/java/com/example/divyang/carpark/MainActivity.java
@@ -14,41 +14,50 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    private Button enter,exit;
-    private DatabaseReference reference;
+    private Button enter,exit,book,parked;
+    private DatabaseReference reference,mainReference;
     private TextView number,status;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        enter = (Button)findViewById(R.id.enter);
-        exit = (Button)findViewById(R.id.exit);
+        enter = (Button) findViewById(R.id.enter);
+        book = (Button) findViewById(R.id.book);
+        parked = (Button) findViewById(R.id.parked);
+        exit = (Button) findViewById(R.id.exit);
         number = (TextView) findViewById(R.id.number);
         status = (TextView) findViewById(R.id.status);
         reference = FirebaseDatabase.getInstance().getReference().child("parking");//pointing to null
-
+        mainReference = FirebaseDatabase.getInstance().getReference();
 
 
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                carParked(reference);
+
+            }
+        });
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
                 reference.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
 
                         String slots = dataSnapshot.getValue().toString();
                         int slot;
-                        slot  = Integer.parseInt(slots);
-                        if(slot == 60)
-                        {
-                            Toast.makeText(MainActivity.this,"Already FULL",Toast.LENGTH_LONG).show();
-                        }
-                        else
-                        {
-                            slot++;
+                        slot = Integer.parseInt(slots);
+                        if (slot == 0) {
+                            Toast.makeText(MainActivity.this, "Empty", Toast.LENGTH_LONG).show();
+                        } else {
+                            slot--;
                             slots = String.valueOf(slot);
                             reference.setValue(slots);
                         }
@@ -65,72 +74,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-exit.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View view) {
-
-        reference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                String slots = dataSnapshot.getValue().toString();
-                int slot;
-                slot  = Integer.parseInt(slots);
-                if(slot == 0)
-                {
-                    Toast.makeText(MainActivity.this,"Empty",Toast.LENGTH_LONG).show();
-                }
-                else
-                {
-                    slot--;
-                    slots = String.valueOf(slot);
-                    reference.setValue(slots);
-                }
-
-            }
-
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-
-
-
-
-
-
-
-
-
-    }
-});
-
-
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 String slots = dataSnapshot.getValue().toString();
                 int slot;
-                slot  = Integer.parseInt(slots);
+                slot = Integer.parseInt(slots);
                 number.setText(slots);
-                if(slot==60)
-                {
+                if (slot == 60) {
                     status.setText("FULL");
                     number.setTextColor(Color.RED);
                     status.setTextColor(Color.RED);
-                }
-                else
-                {
+                } else {
                     status.setText("Available");
                     number.setTextColor(Color.GREEN);
                     status.setTextColor(Color.GREEN);
                 }
-
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
@@ -138,10 +100,54 @@ exit.setOnClickListener(new View.OnClickListener() {
         });
 
 
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Long tsLong = System.currentTimeMillis() / 1000;
+                String ts = tsLong.toString();
+                mainReference.child("timestamp").setValue(ServerValue.TIMESTAMP);
+
+                Toast.makeText(MainActivity.this, "booked", Toast.LENGTH_SHORT).show();
+                carParked(reference);
+            }
+        });
+    }
         //check internet connection
+
+
+        void carParked(final DatabaseReference references){
+
+            references.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    String slots = dataSnapshot.getValue().toString();
+                    int slot;
+                    slot = Integer.parseInt(slots);
+                    if (slot == 60) {
+                        Toast.makeText(MainActivity.this, "Already FULL", Toast.LENGTH_LONG).show();
+                    } else {
+                        slot++;
+                        slots = String.valueOf(slot);
+                        references.setValue(slots);
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
+    }
+
 
     }
 
 
 
-}
+
+
