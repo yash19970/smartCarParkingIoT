396b5e965e7e7071ebe9f6686ae5b65b8f3b7598
Your Name <divyang@mediaiqdigital.com>
2018-03-17 15:27:42 +0530

    stream code changed

--- hardware/stream.py
+++ hardware/stream.py
@@ -2,14 +2,14 @@ import serial
 import time
 import pyrebase
 from time import gmtime, strftime	
-ard = serial.Serial("/dev/ttyACM1",9600)
+ard = serial.Serial("/dev/ttyACM0",9600)
 config = {
     "apiKey": "AIzaSyD01mwCYWJ4x-grwrAhZh2fSesfToH_QVA",
     "authDomain": "carpark-9c5d5.firebaseapp.com",
     "databaseURL": "https://carpark-9c5d5.firebaseio.com",
     "projectId": "carpark-9c5d5",
     "storageBucket": "carpark-9c5d5.appspot.com",
-    "serviceAccount": "/home/yash/Downloads/CarPark-1c0acc79bed3.json"
+    "serviceAccount": "/home/divyang/Desktop/pbl/smartCarParkIOT/hardware/CarPark-1c0acc79bed3.json"
 }
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
@@ -29,4 +29,4 @@ def stream_handler(message):
     	ard.write((message["data"].encode()))
 
 
-my_stream = db.child("sensor").stream(stream_handler)
+my_stream = db.child("Location").child("Phoenix MarketCity, Velacherry").child("Sensor").stream(stream_handler)
