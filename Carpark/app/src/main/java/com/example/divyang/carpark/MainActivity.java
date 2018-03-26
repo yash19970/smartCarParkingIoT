c6bf3d2c1c013ba993dfb9f9a450725bc0035b62
Gauri Dhawan <gauri@sheroes.in>
2018-03-26 10:32:57 +0530

    including google maps - not complete

--- Carpark/app/src/main/java/com/example/divyang/carpark/MainActivity.java
+++ Carpark/app/src/main/java/com/example/divyang/carpark/MainActivity.java
@@ -23,6 +23,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -148,6 +152,9 @@ public class MainActivity extends Fragment {
 
             }
         });
+
+
+
         return view;
     }
 
